47f2a80aead4990ab7f973478959bf21ff3703c3
lokeshdurgi <llokeshdurgi@gmail.com>
2026-07-27 18:54:59 +0530

    Add files via upload

---
+++ Week-1/Patterns and Principles/strategy pattern/PaymentContext.java
@@ -0,0 +1,17 @@
+package strategypattern;
+
+public class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        if (paymentStrategy == null) {
+            System.out.println("No payment strategy selected!");
+        } else {
+            paymentStrategy.pay(amount);
+        }
+    }
+}
