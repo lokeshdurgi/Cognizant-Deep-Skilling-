7cf94a07b16b9412ffee9e805040f216507566b9
lokeshdurgi <llokeshdurgi@gmail.com>
2026-07-27 21:54:16 +0530

    Add files via upload

---
+++ Week-2/Employee Management System/scr/main/java/com/cogizant/Model/Department.java
@@ -0,0 +1,21 @@
+package com.example.employeemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
