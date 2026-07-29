f845026433557032c9e1a865eb81fd1550db6bd8
lokeshdurgi <llokeshdurgi@gmail.com>
2026-07-29 20:57:44 +0530

    Add files via upload

---
+++ Week-2/spring jpa and hibernate - 1/orm-learn/scr/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+        countryService = context.getBean(CountryService.class);
+        testGetAllCountries();
+    }
+
+    private static void testGetAllCountries() {
+        LOGGER.info("Start");
+        LOGGER.debug("countries={}", countryService.getAllCountries());
+        LOGGER.info("End");
+    }
+
+}
