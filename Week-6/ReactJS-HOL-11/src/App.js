737a186aa5b7cb951c06f56564000980376ccac1
lokeshdurgi <llokeshdurgi@gmail.com>
2026-07-30 20:38:13 +0530

    Add files via upload

---
+++ Week-6/ReactJS-HOL-11/src/App.js
@@ -0,0 +1,15 @@
+// HOL 10 - Forms in React
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>HOL 10 - Forms in React</h1>
+      <hr />
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
